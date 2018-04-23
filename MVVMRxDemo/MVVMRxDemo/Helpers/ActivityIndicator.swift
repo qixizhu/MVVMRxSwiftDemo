be855671bb67e86908cb258f4dac80d16b1784bf
qixizhu <hanqi_ah@163.com>
2018-04-23 13:59:38 +0800

    Variable to BehaviorRelay
    ActivityIndicator.swift 中Variable 换成
     BehaviorRelay。

--- MVVMRxDemo/MVVMRxDemo/Helpers/ActivityIndicator.swift
+++ MVVMRxDemo/MVVMRxDemo/Helpers/ActivityIndicator.swift
@@ -38,8 +38,8 @@ public class ActivityIndicator : SharedSequenceConvertibleType {
     public typealias SharingStrategy = DriverSharingStrategy
 
     private let _lock = NSRecursiveLock()
-//    private let _variable = BehaviorRelay(value: 0)
-    private let _variable = Variable(0)
+    private let _variable = BehaviorRelay(value: 0)
+//    private let _variable = Variable(0)
     private let _loading: SharedSequence<SharingStrategy, Bool>
 
     public init() {
@@ -59,15 +59,15 @@ public class ActivityIndicator : SharedSequenceConvertibleType {
 
     private func increment() {
         _lock.lock()
-//        _variable.accept(_variable.value + 1)
-        _variable.value = _variable.value + 1
+        _variable.accept(_variable.value + 1)
+//        _variable.value = _variable.value + 1
         _lock.unlock()
     }
 
     private func decrement() {
         _lock.lock()
-//        _variable.accept(_variable.value - 1)
-        _variable.value = _variable.value - 1
+        _variable.accept(_variable.value - 1)
+//        _variable.value = _variable.value - 1
         _lock.unlock()
     }
 
